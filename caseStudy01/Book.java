84912889a4f709440f37d78f256ef7031c46b83c
unknown <superbigthui2002@gmail.com>
2022-09-19 17:02:16 +0700

    first commit

---
+++ caseStudy01/Book.java
@@ -0,0 +1,51 @@
+package demoChuong03.caseStudy01;
+
+public class Book {
+	private String name;
+	private Author author;
+	private double price;
+	private int qty;
+
+	public Book(String name, Author author, double price) {
+		this.name = name;
+		this.author = author;
+		setPrice(price);
+		this.qty = 0;
+	}
+
+	public Book(String name, Author author, double price, int qty) {
+		this.name = name;
+		this.author = author;
+		setPrice(price);
+		setQty(qty);
+	}
+
+	public double getPrice() {
+		return price;
+	}
+
+	public void setPrice(double price) {
+		this.price = price;
+	}
+
+	public int getQty() {
+		return qty;
+	}
+
+	public void setQty(int qty) {
+		this.qty = qty;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public Author getAuthor() {
+		return author;
+	}
+
+	@Override
+	public String toString() {
+		return String.format("Book[name=%s,Author[%s],price=%f, qty=%d]", name, author, price, qty);
+	}
+}
---
+++ caseStudy01/Test.java
@@ -0,0 +1,14 @@
+package demoChuong03.caseStudy01;
+
+public class Test {
+
+	public static void main(String[] args) {
+//		Author au=new Author("Hao", "hao@gmail.com", 'M');
+//		System.out.println(au);
+		
+		Book b=new Book("LT HDT", new Author("An", "an@gmail.com", 'F'), 100000, 10);
+		System.out.println(b);
+		
+	}
+
+}
